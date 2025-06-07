b5008b7d4ecf060c9bec10ae004f837b0e09075a
Sayan <dassayan004@gmail.com>
2025-06-07 17:39:49 +0530

    up

---
+++ lib/core/app.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intelligenz/core/services/auth/auth_repository.dart';
+import 'package:intelligenz/core/services/auth/cubit/auth_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:intelligenz/core/utils/route.dart';
+
+Future<Widget> initializeApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GoogleFonts.pendingFonts([
+    GoogleFonts.dmSans(),
+    GoogleFonts.secularOne(),
+  ]);
+
+  return const MyApp();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final authRepository = AuthRepository();
+    final authCubit = AuthCubit(authRepository);
+
+    final GoRouter goRouter = router(authCubit);
+
+    return MultiBlocProvider(
+      providers: [BlocProvider<AuthCubit>.value(value: authCubit)],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        routeInformationProvider: goRouter.routeInformationProvider,
+        routerDelegate: goRouter.routerDelegate,
+        routeInformationParser: goRouter.routeInformationParser,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:intelligenz/core/services/auth/auth_repository.dart';
-import 'package:intelligenz/core/services/auth/cubit/auth_cubit.dart';
-import 'package:intelligenz/core/utils/route.dart';
+import 'package:intelligenz/core/app.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final authRepository = AuthRepository();
-    final authCubit = AuthCubit(authRepository);
-
-    final GoRouter goRouter = router(authCubit);
-
-    return MultiBlocProvider(
-      providers: [BlocProvider<AuthCubit>.value(value: authCubit)],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        routeInformationProvider: goRouter.routeInformationProvider,
-        routerDelegate: goRouter.routerDelegate,
-        routeInformationParser: goRouter.routeInformationParser,
-      ),
-    );
-  }
-}
+void main() async => runApp(await initializeApp());
